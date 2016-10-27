0fae7836b73e78cb30704ba441a06cb5187bd8d5
Georg Ledermann <mail@georg-ledermann.de>
2016-10-27 15:03:24 +0200

    Add Sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+require 'sidekiq/api'
+
+redis_config = { url: ENV['REDIS_URL'] }
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_config
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#index"
+
+  mount Sidekiq::Web => '/sidekiq'
 end
