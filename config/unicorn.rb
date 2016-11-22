44936ceeeb287f1ed13f5e91d2cbe30df10e5711
Georg Ledermann <mail@georg-ledermann.de>
2016-11-22 10:25:22 +0100

    :whale: Log to stdout/stderr

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,8 @@ working_directory app_dir
 
 pid "#{app_dir}/tmp/unicorn.pid"
 
-# stderr_path "#{app_dir}/log/unicorn.stderr.log"
-# stdout_path "#{app_dir}/log/unicorn.stdout.log"
+stderr_path "/dev/stderr"
+stdout_path "/dev/stdout"
 
 worker_processes 3
 listen "/tmp/unicorn.sock", :backlog => 64
