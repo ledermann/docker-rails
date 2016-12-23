54818f12bf3db6604cb62a3cb1bfa377300b91ff
Georg Ledermann <mail@georg-ledermann.de>
2016-12-23 09:39:31 +0100

    :wrench: Use Sidekiq in production only

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module DockerRails
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.active_job.queue_adapter = :sidekiq
-
     config.middleware.use PDFKit::Middleware
     config.asset_host = Proc.new { |source, request|
       if request && request.env['Rack-Middleware-PDFKit']
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "docker-rails_#{Rails.env}"
+  config.active_job.queue_adapter = :sidekiq
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
