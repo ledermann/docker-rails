9f557bde8c4edd779d3eb9f59fef465e3f4a2ac6
Georg Ledermann <mail@georg-ledermann.de>
2017-03-23 06:15:37 +0100

    JS: Use jQuery 3

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
