a46a2c906a635d881a12831697a68aa5183a86b0
Georg Ledermann <mail@georg-ledermann.de>
2017-05-05 11:16:01 +0200

    rake app:update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,16 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  # Mount Action Cable outside main process or domain
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  if ENV['APP_HOST']
+    config.action_cable.allowed_request_origins = [/(?:^(http|https):\/\/)?(?:([^.]+)\.)?#{ENV['APP_HOST']}/]
+  else
+    # Allow all origins
+    config.action_cable.disable_request_forgery_protection = true
+  end
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -56,7 +66,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "docker-rails_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "docker_rails_#{Rails.env}"
   config.active_job.queue_adapter = :sidekiq
 
   config.action_mailer.perform_caching = false
@@ -87,11 +97,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  if ENV['APP_HOST']
-    config.action_cable.allowed_request_origins = [/(?:^(http|https):\/\/)?(?:([^.]+)\.)?#{ENV['APP_HOST']}/]
-  else
-    # Allow all origins
-    config.action_cable.disable_request_forgery_protection = true
-  end
 end
