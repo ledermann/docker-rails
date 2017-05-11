220bb67fdeb0cf01a71994506d016a65e85fe02a
Georg Ledermann <mail@georg-ledermann.de>
2017-05-11 12:49:01 +0200

    Tests: Enable view rendering in controller specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.render_views
 end
