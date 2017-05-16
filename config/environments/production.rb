50eefe043f86533255ba734b0ae9b5033d673d24
Georg Ledermann <mail@georg-ledermann.de>
2017-05-16 12:31:42 +0200

    No need for ActionMailer in this app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "docker_rails_#{Rails.env}"
   config.active_job.queue_adapter = :sidekiq
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
