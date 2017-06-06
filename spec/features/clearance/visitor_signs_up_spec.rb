25d0ceb3a259fc702bb324d26b89ad10cfc5232b
Georg Ledermann <mail@georg-ledermann.de>
2017-06-06 06:38:07 +0200

    :white_check_mark: Add more specs

--- spec/features/clearance/visitor_signs_up_spec.rb
+++ spec/features/clearance/visitor_signs_up_spec.rb
@@ -19,6 +19,16 @@ feature "Visitor signs up" do
     expect(page).to have_content I18n.t("flashes.confirmed_registration")
   end
 
+  scenario "tries with confirmation twice" do
+    sign_up_with "valid@example.com", "password"
+
+    open_email "valid@example.com"
+    click_first_link_in_email
+    click_first_link_in_email
+
+    expect(page).to have_content I18n.t("flashes.already_confirmed")
+  end
+
   scenario "tries with invalid email" do
     sign_up_with "invalid_email", "password"
 
@@ -29,5 +39,23 @@ feature "Visitor signs up" do
     sign_up_with "valid@example.com", ""
 
     expect_user_to_be_signed_out
+  end
+
+  scenario "tries with wrong token" do
+    sign_up_with "valid@example.com", "password"
+
+    user = User.last
+    visit "/confirm/#{user.id}/wrong-token"
+
+    expect(page).to have_content I18n.t("flashes.failure_confirmation")
+  end
+
+  scenario "tries with wrong user_id" do
+    sign_up_with "valid@example.com", "password"
+
+    user = User.last
+    visit "/confirm/000/#{user.email_confirmation_token}"
+
+    expect(page).to have_content I18n.t("flashes.failure_confirmation_user")
   end
 end
