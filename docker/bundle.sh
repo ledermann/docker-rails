1b03430e9d999d490f796c0e13e6193dae14a1b1
Georg Ledermann <mail@georg-ledermann.de>
2017-06-09 07:59:29 +0200

    :whale: Bundle install without development

--- docker/bundle.sh
+++ docker/bundle.sh
@@ -7,7 +7,7 @@ N=0
 STATUS=1
 until [ ${N} -ge 5 ]
 do
-  bundle install --jobs 20 && STATUS=0 && break
+  bundle install --without development --jobs 20 && STATUS=0 && break
   echo 'Try bundle again ...'
   N=$[${N}+1]
   sleep 1
