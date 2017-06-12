098984d5301a98ed4c1a65a354bd3de2759d12f7
Georg Ledermann <mail@georg-ledermann.de>
2017-06-12 19:31:01 +0200

    List styling

--- spec/views/posts/index_spec.rb
+++ spec/views/posts/index_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 describe 'posts/index' do
   before :each do
-    2.times { create(:post, title: 'Foo') }
+    2.times { create(:post, title: 'Foo', content: 'Lorem ipsum') }
     assign :posts, Post.all.page(1)
   end
 
   it 'renders a list of posts' do
     render
-    assert_select 'tr > td', text: 'Foo', count: 2
+    assert_select 'tr > td', text: 'Foo Lorem ipsum', count: 2
   end
 end
