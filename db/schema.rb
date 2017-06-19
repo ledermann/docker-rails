db4d789d253613b36ba5aa52e78aa57ab190673a
Georg Ledermann <mail@georg-ledermann.de>
2017-06-19 08:39:18 +0200

    PaperTrail: Add #object_changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618082024) do
+ActiveRecord::Schema.define(version: 20170619061723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170618082024) do
     t.string "whodunnit"
     t.text "object"
     t.datetime "created_at"
+    t.text "object_changes"
     t.index ["item_type", "item_id"], name: "index_audits_on_item_type_and_item_id"
   end
 
