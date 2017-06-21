03cda4b4166429dc88fc10ea534db5f7b9bae41f
Georg Ledermann <mail@georg-ledermann.de>
2017-06-21 13:39:11 +0200

    Fix test not being executed

--- spec/helpers/bootstrap_helper_spec.rb
+++ spec/helpers/bootstrap_helper_spec.rb
@@ -4,6 +4,6 @@ describe BootstrapHelper do
   describe 'link_to_active' do
     subject { helper.link_to_active 'foo', '/foo' }
 
-    it { eq('<a href="/foo">foo</a>') }
+    it { expect(subject).to eq('<a href="/foo">foo</a>') }
   end
 end
