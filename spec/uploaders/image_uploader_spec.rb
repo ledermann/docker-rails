0078d9c94201d5ceed2243ae87a39fdd4feb5371
Georg Ledermann <mail@georg-ledermann.de>
2017-06-25 11:48:13 +0200

    :white_check_mark: More Clip testing

--- spec/uploaders/image_uploader_spec.rb
+++ spec/uploaders/image_uploader_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ImageUploader do
   let(:post) { create :post }
-  let(:clip) { post.clips.create image: fixture_file_upload('spec/fixtures/example.jpg') }
+  let(:clip) { create :clip, post: post }
 
   it 'generates image versions' do
     expect(clip.reload.image.keys).to eq([:original, :thumbnail])
@@ -10,10 +10,27 @@ describe ImageUploader do
 
   it 'stores image' do
     expect(clip.reload.image[:original].storage_key).to eq('store')
+    expect(clip.reload.image[:thumbnail].storage_key).to eq('store')
   end
 
-  it 'stores mime type' do
-    expect(clip.reload.image[:original].mime_type).to eq('image/jpeg')
+  describe "metadata" do
+    subject { clip.reload.image[:original] }
+
+    it 'extracts mime type' do
+      expect(subject.mime_type).to eq('image/jpeg')
+    end
+
+    it "extracts filesize" do
+      expect(subject.size).to eq(8826)
+    end
+
+    it "extracts original filename" do
+      expect(subject.original_filename).to eq('example.jpg')
+    end
+
+    it "extracts extension" do
+      expect(subject.extension).to eq('jpg')
+    end
   end
 
   it 'rejects other files than images' do
