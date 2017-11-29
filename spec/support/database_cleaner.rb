14b21badc4c2fc27e74462052eb776f7856d74b4
Georg Ledermann <mail@georg-ledermann.de>
2017-11-29 08:40:01 +0100

    DatabaseCleaner: Just check for :js to choose strategy

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -8,17 +8,8 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before :each, type: :system do
-    # :rack_test driver's Rack app under test shares database connection
-    # with the specs, so continue to use transaction strategy for speed.
-    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
-
-    unless driver_shares_db_connection_with_specs
-      # Driver is probably for an external browser with an app
-      # under test that does *not* share a database connection with the
-      # specs, so use truncation strategy.
-      DatabaseCleaner.strategy = :truncation
-    end
+  config.before :each, js: true do
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before :each do
