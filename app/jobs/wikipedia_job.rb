c1909cb6f812d179ec951c40e2e53a4b0c8ea1e4
Georg Ledermann <mail@georg-ledermann.de>
2017-12-30 09:06:45 +0100

    :art: WikipediaJob: Refactoring to improve code

--- app/jobs/wikipedia_job.rb
+++ app/jobs/wikipedia_job.rb
@@ -3,7 +3,7 @@ require 'wikipedia'
 class WikipediaJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(*)
     PaperTrail.whodunnit = 'Wikipedia'
     Searchkick.callbacks(:bulk) do
       count = 0
@@ -11,22 +11,28 @@ class WikipediaJob < ApplicationJob
         next unless article.valid?
         logger.debug "Processing '#{article.title}'"
 
-        short_title = article.title.remove(' (programming language)')
-        post = Post.find_or_initialize_by(title: short_title)
-        post.content = article.extract
-        post.copyright = <<-HTML
-          <p>This article is based on the article <a rel="nofollow" href="#{article.html_url}">#{article.title}</a> from the free encyclopedia <a rel="nofollow" href="https://en.wikipedia.org/wiki/Main_Page">Wikipedia</a> and is licened under <a rel="nofollow" href="http://www.gnu.org/licenses/fdl-1.3.txt">GNU Free Documentation License</a> and <a rel="nofollow" href="https://creativecommons.org/licenses/by-sa/3.0/">Creative Commons CC-BY-SA 3.0 Unported</a> (<a rel="nofollow" href="https://creativecommons.org/licenses/by-sa/3.0/legalcode">Summary</a>). In Wikipedia there is a <a rel="nofollow" href="#{article.history_url}">List of authors</a> available.</p>
-        HTML
-        post.copyright.strip!
+        post = build_post(article)
+        next unless post.changed?
 
-        if post.changed?
-          logger.info "Updating post '#{post.title}'"
-          post.save!
-          count += 1
-        end
+        logger.info "Updating post '#{post.title}'"
+        post.save!
+        count += 1
       end
 
       logger.info "Updated #{count} posts"
     end
+  end
+
+  private
+
+  def build_post(article)
+    short_title = article.title.remove(' (programming language)')
+    post = Post.find_or_initialize_by(title: short_title)
+    post.content = article.extract
+    post.copyright = <<-HTML
+      <p>This article is based on the article <a rel="nofollow" href="#{article.html_url}">#{article.title}</a> from the free encyclopedia <a rel="nofollow" href="https://en.wikipedia.org/wiki/Main_Page">Wikipedia</a> and is licened under <a rel="nofollow" href="http://www.gnu.org/licenses/fdl-1.3.txt">GNU Free Documentation License</a> and <a rel="nofollow" href="https://creativecommons.org/licenses/by-sa/3.0/">Creative Commons CC-BY-SA 3.0 Unported</a> (<a rel="nofollow" href="https://creativecommons.org/licenses/by-sa/3.0/legalcode">Summary</a>). In Wikipedia there is a <a rel="nofollow" href="#{article.history_url}">List of authors</a> available.</p>
+    HTML
+    post.copyright.strip!
+    post
   end
 end
