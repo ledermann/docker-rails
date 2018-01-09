dbd56cdfd4c15f13f524fe866e8817c9d02b9fc3
Georg Ledermann <mail@georg-ledermann.de>
2018-01-09 12:20:56 +0100

    rails generate react:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
