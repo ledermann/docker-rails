62137d5917e5bf589fcf40c952eee37286b4d889
Georg Ledermann <mail@georg-ledermann.de>
2018-01-10 11:02:05 +0100

    Rails 5.2 schema version number is formatted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816073804) do
+ActiveRecord::Schema.define(version: 2017_08_16_073804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
