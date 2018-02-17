5a433058163bb7b6a642934b843479b466f94e12
Georg Ledermann <mail@georg-ledermann.de>
2018-02-17 07:20:27 +0100

    Fix flaky test

--- spec/models/clip_spec.rb
+++ spec/models/clip_spec.rb
@@ -19,7 +19,7 @@ describe Clip, 'created for Post' do
   it "touches post" do
     clip
     post.reload
-    expect(post.updated_at.to_s).to eq(clip.created_at.to_s)
+    expect(post.updated_at).to be_within(1.second).of clip.created_at
   end
 
   it "increases counter cache" do
