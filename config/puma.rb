7ddb5df9ad635290b84c2d4edca1d283b43cc75e
Georg Ledermann <mail@georg-ledermann.de>
2018-04-26 10:08:08 +0200

    Puma: With Rails 5.2.0, ActiveRecord is fork-proof
    https://github.com/rails/rails/pull/31241

--- config/puma.rb
+++ config/puma.rb
@@ -32,24 +32,5 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 #
 preload_app!
 
-# If you are preloading your application and using Active Record, it's
-# recommended that you close any connections to the database before workers
-# are forked to prevent connection leakage.
-#
-before_fork do
-  ActiveRecord::Base.connection_pool.disconnect! if defined?(ActiveRecord)
-end
-
-# The code in the `on_worker_boot` will be called if you are using
-# clustered mode by specifying a number of `workers`. After each worker
-# process is booted, this block will be run. If you are using the `preload_app!`
-# option, you will want to use this block to reconnect to any threads
-# or connections that may have been created at application boot, as Ruby
-# cannot share connections between processes.
-#
-on_worker_boot do
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
-
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
