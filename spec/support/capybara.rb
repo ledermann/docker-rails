eefd3ffd6daddfb23ed319ea5da8f62765f2d453
Georg Ledermann <mail@georg-ledermann.de>
2019-06-09 09:12:29 +0200

    Capybara: Fix using Chrome 75 by disabling W3C mode
    Reverts f6b2dc7

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -15,11 +15,20 @@ RSpec.configure do |config|
   config.before :each, type: :system, js: true do
     if ENV['SELENIUM_REMOTE_HOST']
       # Docker environment
-      driven_by :selenium, using: :chrome, options: {
-        browser: :remote,
-        url: "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub",
-        desired_capabilities: :chrome
-      }
+      driven_by(
+        :selenium,
+        using: :chrome,
+        options: {
+          browser: :remote,
+          url: "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub",
+          desired_capabilities: {
+            chromeOptions: {
+              args: %w[headless],
+              w3c: false
+            }
+          }
+        }
+      )
 
       # Find Docker IP address
       docker_ip = `/sbin/ip route|awk '/scope/ { print $7 }'`.strip
@@ -28,22 +37,31 @@ RSpec.configure do |config|
       Capybara.app_host = "http://#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}"
     else
       # Local environment
-      driven_by :selenium_chrome_headless
+      driven_by(
+        :selenium,
+        using: :chrome,
+        options: {
+          desired_capabilities: {
+            chromeOptions: {
+              args: %w[headless],
+              w3c: false
+            }
+          }
+        }
+      )
     end
   end
 
   config.after :each, type: :system, js: true do
-    # Temporary disabled because of issues with Chrome 75
-    #
-    # page.driver.browser.manage.logs.get(:browser).each do |log|
-    #   case log.message
-    #   when /This page includes a password or credit card input in a non-secure context/
-    #     # Ignore this warning in tests
-    #     next
-    #   else
-    #     message = "[#{log.level}] #{log.message}"
-    #     raise message
-    #   end
-    # end
+    page.driver.browser.manage.logs.get(:browser).each do |log|
+      case log.message
+      when /This page includes a password or credit card input in a non-secure context/
+        # Ignore this warning in tests
+        next
+      else
+        message = "[#{log.level}] #{log.message}"
+        raise message
+      end
+    end
   end
 end
