ed142b639aa96d1ab2068e63be8e13052ffe8f63
Georg Ledermann <mail@georg-ledermann.de>
2019-08-02 06:18:54 +0200

    :arrow_up: Upgrade gem "rubocop" to 0.74.0

--- app/helpers/piwik_helper.rb
+++ app/helpers/piwik_helper.rb
@@ -1,7 +1,7 @@
 module PiwikHelper
   def piwik_meta_tags
     if (piwik_host = ENV['PIWIK_HOST']) &&
-       (piwik_id   = ENV['PIWIK_ID'])
+       (piwik_id = ENV['PIWIK_ID'])
       safe_join [
         tag(:meta, name: 'piwik-host', content: piwik_host),
         tag(:meta, name: 'piwik-id',   content: piwik_id)
