f72bf128b114e06e3e9815c99eb0e778d8eed516
Georg Ledermann <mail@georg-ledermann.de>
2020-04-20 05:33:45 +0200

    Webpacker: There is no yarn integrity check anymore
    Adopt changes from Webpacker 5.1.0
    
    https://github.com/rails/webpacker/pull/2518

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Verifies that versions and hashed value of the package contents in the project's package.json
-  config.webpacker.check_yarn_integrity = true
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
