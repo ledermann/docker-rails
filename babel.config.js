7e998b18f7c4e70a7af91c83350106e7f4bff706
Georg Ledermann <georg@ledermann.dev>
2021-05-06 06:41:00 +0200

    :arrow_up: yarn upgrade

--- babel.config.js
+++ babel.config.js
@@ -41,12 +41,7 @@ module.exports = function(api) {
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
-      [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
+      '@babel/plugin-proposal-class-properties',
       [
         '@babel/plugin-proposal-object-rest-spread',
         {
