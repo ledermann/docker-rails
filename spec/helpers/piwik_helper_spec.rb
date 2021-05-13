f33d500e51d350ecbc33b57ec3065fd523ff300e
Georg Ledermann <georg@ledermann.dev>
2021-05-13 14:08:21 +0200

    Fix RuboCop warnings

--- spec/helpers/piwik_helper_spec.rb
+++ spec/helpers/piwik_helper_spec.rb
@@ -6,8 +6,8 @@ describe PiwikHelper do
 
     context "with env vars defined" do
       before do
-        allow(ENV).to receive(:[]).with("PIWIK_HOST").and_return("piwik.example.org")
-        allow(ENV).to receive(:[]).with("PIWIK_ID").and_return("42")
+        allow(Rails.configuration.x).to receive(:piwik_host).and_return("piwik.example.org")
+        allow(Rails.configuration.x).to receive(:piwik_id).and_return("42")
       end
 
       it { expect(subject.lines.first.chomp).to  eq('<meta name="piwik-host" content="piwik.example.org" />') }
