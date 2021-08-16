f328792ed57c7f4738f2899f51b31f080e924ba5
Georg Ledermann <georg@ledermann.dev>
2021-08-16 07:45:48 +0200

    Add missing quotes in bash scripts

--- docker/wait-for-services.sh
+++ docker/wait-for-services.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Wait for PostgreSQL
-until nc -z -v -w30 $DB_HOST 5432
+until nc -z -v -w30 "$DB_HOST" 5432
 do
   echo 'Waiting for PostgreSQL...'
   sleep 1
@@ -9,7 +9,7 @@ done
 echo "PostgreSQL is up and running"
 
 # Wait for Elasticsearch
-until nc -z -v -w30 $ELASTICSEARCH_HOST 9200
+until nc -z -v -w30 "$ELASTICSEARCH_HOST" 9200
 do
   echo 'Waiting for Elasticsearch...'
   sleep 1
