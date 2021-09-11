56994fe910e0e8e8f5ac0a2099730c17c33426fe
Georg Ledermann <georg@ledermann.dev>
2021-09-11 11:14:10 +0200

    Fix RuboCop warnings

--- app/helpers/piwik_helper.rb
+++ app/helpers/piwik_helper.rb
@@ -3,8 +3,8 @@ module PiwikHelper
     if (piwik_host = Rails.configuration.x.piwik_host) &&
        (piwik_id = Rails.configuration.x.piwik_id)
       safe_join [
-        tag(:meta, name: 'piwik-host', content: piwik_host),
-        tag(:meta, name: 'piwik-id',   content: piwik_id)
+        tag.meta(name: 'piwik-host', content: piwik_host),
+        tag.meta(name: 'piwik-id',   content: piwik_id)
       ], "\n"
     end
   end
