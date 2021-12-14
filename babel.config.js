88ab45b54e068091a9fbdc0877dc49b9e234a7cb
Georg Ledermann <georg@ledermann.dev>
2021-12-14 09:49:00 +0100

    Fix P42 warnings

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,18 @@
-module.exports = function(api) {
-  var validEnv = ['development', 'test', 'production']
-  var currentEnv = api.env()
-  var isDevelopmentEnv = api.env('development')
-  var isProductionEnv = api.env('production')
-  var isTestEnv = api.env('test')
+module.exports = (api) => {
+  const validEnv = ['development', 'test', 'production'];
+  const currentEnv = api.env();
+  const isDevelopmentEnv = api.env('development');
+  const isProductionEnv = api.env('production');
+  const isTestEnv = api.env('test');
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
       'Please specify a valid `NODE_ENV` or ' +
-        '`BABEL_ENV` environment variables. Valid values are "development", ' +
-        '"test", and "production". Instead, received: ' +
-        JSON.stringify(currentEnv) +
-        '.'
-    )
+      '`BABEL_ENV` environment variables. Valid values are "development", ' +
+      '"test", and "production". Instead, received: ' +
+      JSON.stringify(currentEnv) +
+      '.'
+    );
   }
 
   return {
@@ -61,5 +61,5 @@ module.exports = function(api) {
         }
       ]
     ].filter(Boolean)
-  }
+  };
 }
